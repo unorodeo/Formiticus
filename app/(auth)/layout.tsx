840bb3e97db3bd47c0b17697e7f6d1bd1165cb1e
enoabasi <enoabasidream@gmail.com>
2024-11-18 04:29:56 +0100

    refac/remove `prisma` + remove `auth` routes

--- app/(auth)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from "react";
-
-export default function AuthLayout({ children }: { children: ReactNode }) {
-	return <main className="relative grid flex-1 p-2 bg-surface place-items-center">{children}</main>;
-}
